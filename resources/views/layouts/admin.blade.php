111b04781661b7adf16a1d404079d0e56c98276c
Harbecox <harbecox@gmail.com>
2022-12-07 10:19:24 +0400

    small fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -34,80 +34,80 @@
         </ul>
 
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                    <i class="fas fa-search"></i>
-                </a>
-                <div class="navbar-search-block">
-                    <form class="form-inline">
-                        <div class="input-group input-group-sm">
-                            <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-                            <div class="input-group-append">
-                                <button class="btn btn-navbar" type="submit">
-                                    <i class="fas fa-search"></i>
-                                </button>
-                                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                                    <i class="fas fa-times"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </li>
+{{--            <li class="nav-item">--}}
+{{--                <a class="nav-link" data-widget="navbar-search" href="#" role="button">--}}
+{{--                    <i class="fas fa-search"></i>--}}
+{{--                </a>--}}
+{{--                <div class="navbar-search-block">--}}
+{{--                    <form class="form-inline">--}}
+{{--                        <div class="input-group input-group-sm">--}}
+{{--                            <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">--}}
+{{--                            <div class="input-group-append">--}}
+{{--                                <button class="btn btn-navbar" type="submit">--}}
+{{--                                    <i class="fas fa-search"></i>--}}
+{{--                                </button>--}}
+{{--                                <button class="btn btn-navbar" type="button" data-widget="navbar-search">--}}
+{{--                                    <i class="fas fa-times"></i>--}}
+{{--                                </button>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </form>--}}
+{{--                </div>--}}
+{{--            </li>--}}
 
-            <li class="nav-item dropdown">
-                <a class="nav-link" data-toggle="dropdown" href="#">
-                    <i class="far fa-comments"></i>
-                    <span class="badge badge-danger navbar-badge">3</span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                    <a href="#" class="dropdown-item">
-                        <div class="media">
-                            <img src="/admin/dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-                            <div class="media-body">
-                                <h3 class="dropdown-item-title">
-                                    Brad Diesel
-                                    <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                                </h3>
-                                <p class="text-sm">Call me whenever you can...</p>
-                                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                            </div>
-                        </div>
-                    </a>
+{{--            <li class="nav-item dropdown">--}}
+{{--                <a class="nav-link" data-toggle="dropdown" href="#">--}}
+{{--                    <i class="far fa-comments"></i>--}}
+{{--                    <span class="badge badge-danger navbar-badge">3</span>--}}
+{{--                </a>--}}
+{{--                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">--}}
+{{--                    <a href="#" class="dropdown-item">--}}
+{{--                        <div class="media">--}}
+{{--                            <img src="/admin/dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">--}}
+{{--                            <div class="media-body">--}}
+{{--                                <h3 class="dropdown-item-title">--}}
+{{--                                    Brad Diesel--}}
+{{--                                    <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>--}}
+{{--                                </h3>--}}
+{{--                                <p class="text-sm">Call me whenever you can...</p>--}}
+{{--                                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </a>--}}
 
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item">
-                        <div class="media">
-                            <img src="/admin/dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-                            <div class="media-body">
-                                <h3 class="dropdown-item-title">
-                                    John Pierce
-                                    <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                                </h3>
-                                <p class="text-sm">I got your message bro</p>
-                                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                            </div>
-                        </div>
-                    </a>
+{{--                    <div class="dropdown-divider"></div>--}}
+{{--                    <a href="#" class="dropdown-item">--}}
+{{--                        <div class="media">--}}
+{{--                            <img src="/admin/dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">--}}
+{{--                            <div class="media-body">--}}
+{{--                                <h3 class="dropdown-item-title">--}}
+{{--                                    John Pierce--}}
+{{--                                    <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>--}}
+{{--                                </h3>--}}
+{{--                                <p class="text-sm">I got your message bro</p>--}}
+{{--                                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </a>--}}
 
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item">
-                        <div class="media">
-                            <img src="/admin/dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-                            <div class="media-body">
-                                <h3 class="dropdown-item-title">
-                                    Nora Silvester
-                                    <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                                </h3>
-                                <p class="text-sm">The subject goes here</p>
-                                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                            </div>
-                        </div>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-                </div>
-            </li>
+{{--                    <div class="dropdown-divider"></div>--}}
+{{--                    <a href="#" class="dropdown-item">--}}
+{{--                        <div class="media">--}}
+{{--                            <img src="/admin/dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">--}}
+{{--                            <div class="media-body">--}}
+{{--                                <h3 class="dropdown-item-title">--}}
+{{--                                    Nora Silvester--}}
+{{--                                    <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>--}}
+{{--                                </h3>--}}
+{{--                                <p class="text-sm">The subject goes here</p>--}}
+{{--                                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </a>--}}
+{{--                    <div class="dropdown-divider"></div>--}}
+{{--                    <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>--}}
+{{--                </div>--}}
+{{--            </li>--}}
             <li class="nav-item dropdown">
                 <a class="nav-link" data-toggle="dropdown" href="#">
                     <i class="far fa-bell"></i>
@@ -134,16 +134,6 @@
                     <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
                 </div>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                    <i class="fas fa-expand-arrows-alt"></i>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
-                    <i class="fas fa-th-large"></i>
-                </a>
-            </li>
         </ul>
     </nav>
 
@@ -157,30 +147,30 @@
             <nav class="mt-2">
                 <ul class="nav nav-pills nav-sidebar flex-column">
                     <li class="nav-item">
-                        <a href="{{ route('admin.accesses.index') }}" class="nav-link">Доступы</a>
+                        <a href="{{ route('admin.accesses.index') }}" class="nav-link @if(request()->is('admin/accesses*')) active @endif">Доступы</a>
                     </li>
                     <li class="nav-item">
-                        <a href="{{ route('admin.users.index') }}" class="nav-link">Пользователи</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a href="{{ route('admin.lectors.index') }}" class="nav-link">Лекторы</a>
+                        <a href="{{ route('admin.users.index') }}" class="nav-link @if(request()->is('admin/users*')) active @endif">Пользователи</a>
                     </li>
 
                     <li class="nav-item">
-                        <a href="{{ route('admin.courses.index') }}" class="nav-link">Курсы</a>
+                        <a href="{{ route('admin.lectors.index') }}" class="nav-link @if(request()->is('admin/lectors*')) active @endif">Лекторы</a>
                     </li>
 
                     <li class="nav-item">
-                        <a href="{{ route('admin.webinars.index') }}" class="nav-link">Вебинары</a>
+                        <a href="{{ route('admin.courses.index') }}" class="nav-link @if(request()->is('admin/courses*')) active @endif">Курсы</a>
                     </li>
 
                     <li class="nav-item">
-                        <a href="{{ route('admin.prices.index') }}" class="nav-link">Цены</a>
+                        <a href="{{ route('admin.webinars.index') }}" class="nav-link @if(request()->is('admin/webinars*')) active @endif">Вебинары</a>
                     </li>
 
                     <li class="nav-item">
-                        <a href="{{ route('admin.certificates.index') }}" class="nav-link">Сертификаты</a>
+                        <a href="{{ route('admin.prices.index') }}" class="nav-link @if(request()->is('admin/prices*')) active @endif">Цены</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="{{ route('admin.certificates.index') }}" class="nav-link @if(request()->is('admin/certificates*')) active @endif">Сертификаты</a>
                     </li>
                 </ul>
             </nav>
