f8a4232b558730a3ac61a50e96a1a0a4628d1d6b
harbecox <harbecox@gmail.com>
2023-03-04 16:58:04 +0400

    seeder fix

--- database/seeders/VideosSeeder.php
+++ database/seeders/VideosSeeder.php
@@ -9,14 +9,15 @@ class VideosSeeder extends Seeder
 {
     public function run()
     {
+        $images = RandomImagesApi::getImages("webinar",7,"webinar");
         $videos = [
-            ['url' => 'https://www.youtube.com/embed/X-kpSmXVuJw'],
-            ['url' => 'https://www.youtube.com/embed/5Ok9oDQgzxs'],
-            ['url' => 'https://www.youtube.com/embed/QPXue2tybcs'],
-            ['url' => 'https://www.youtube.com/embed/X-kpSmXVuJw'],
-            ['url' => 'https://www.youtube.com/embed/5Ok9oDQgzxs'],
-            ['url' => 'https://www.youtube.com/embed/QPXue2tybcs'],
-            ['url' => 'https://www.youtube.com/embed/X-kpSmXVuJw'],
+            ['image' => $images[0],'url' => 'https://www.youtube.com/embed/X-kpSmXVuJw'],
+            ['image' => $images[1],'url' => 'https://www.youtube.com/embed/5Ok9oDQgzxs'],
+            ['image' => $images[2],'url' => 'https://www.youtube.com/embed/QPXue2tybcs'],
+            ['image' => $images[3],'url' => 'https://www.youtube.com/embed/X-kpSmXVuJw'],
+            ['image' => $images[4],'url' => 'https://www.youtube.com/embed/5Ok9oDQgzxs'],
+            ['image' => $images[5],'url' => 'https://www.youtube.com/embed/QPXue2tybcs'],
+            ['image' => $images[6],'url' => 'https://www.youtube.com/embed/X-kpSmXVuJw'],
         ];
         foreach ($videos as $video){
             Video::create($video);
