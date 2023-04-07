25a086b9992c462a9d1afd329dd9c2dd3650f838
harbecox <harbecox@gmail.com>
2023-04-07 12:39:10 +0400

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,7 +9,7 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-         \App\Models\User::factory(30)->create();
+//         \App\Models\User::factory(30)->create();
 //
 //         \App\Models\User::factory()->create([
 //             'name' => 'Test User',
