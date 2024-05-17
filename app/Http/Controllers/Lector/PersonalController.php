5e10076b15502e91f34a82f33a34b82090e36030
NarineHay <naromisho87@gmail.com>
2024-05-17 09:33:34 +0400

    456666

--- app/Http/Controllers/Lector/PersonalController.php
+++ app/Http/Controllers/Lector/PersonalController.php
@@ -12,17 +12,10 @@ class PersonalController extends Controller
 {
     public function index(){
 
-    //    $data = LectorIncome::where('user_id', Auth::id())
-    //    ->select('per_of_sales', 'webinar_id', DB::raw('SUM(total_price) as total_price'), DB::raw('SUM(webinar_price) as webinat_total_price'), DB::raw('COUNT(*) as count'))
-    //    ->groupBy('webinar_id', 'per_of_sales')
-    //    ->get();
-    $data = LectorIncome::where('user_id', Auth::id())->get();
-        $data = $data->pluck('lector_income_currency')->flatten();
-        dd($data);
-        $data = LectorIncome::where('user_id', Auth::id())
-            ->select('per_of_sales', 'item_id', 'type', DB::raw('SUM(total_price) as total_price'), DB::raw('SUM(webinar_price) as webinat_total_price'), DB::raw('COUNT(*) as count'))
-            ->groupBy('item_id', 'type', 'per_of_sales')
-            ->get();
+       $data = LectorIncome::where('user_id', Auth::id())
+       ->select('per_of_sales', 'webinar_id', DB::raw('SUM(total_price) as total_price'), DB::raw('SUM(webinar_price) as webinat_total_price'), DB::raw('COUNT(*) as count'))
+       ->groupBy('webinar_id', 'per_of_sales')
+       ->get();
 
         return view('front.lector.information', compact('data'));
     }
